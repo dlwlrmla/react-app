87234ad1ba82bbd06346e481f3fdb13a3c4df5c7
dlwlrmla <joakomask@gmail.com>
2022-06-19 17:04:43 -0400

    componentes lazy

---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,8 @@
+
+const LazyPage2 = () => {
+  return (
+    <h1>Lazy Page2</h1>
+  )
+}
+
+export default LazyPage2
---
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -0,0 +1,8 @@
+
+const LazyPage3 = () => {
+  return (
+    <h1>Lazy Page3</h1>
+  )
+}
+
+export default LazyPage3
